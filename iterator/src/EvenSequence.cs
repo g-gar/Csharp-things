ed200f9ff3b55fab770b7962e1f402851902479f
g-gar <g.gar.fern@hotmail.com>
2021-10-06 15:58:27 +0200

    added an IEnumerable example

---
+++ iterator/src/EvenSequence.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace iterator{
+    public class EvenSequence : IEnumerable<int>, IEnumerable {
+
+        private readonly int _max;
+
+        public EvenSequence(int max)
+        {
+            _max = max;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            foreach (int i in Enumerable.Range(0, _max).Where(i => i%2 == 0))
+            {
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ iterator/test/EvenSequenceTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace iterator.test{
+    public class EvenSequenceTests{
+
+        [Test]
+        public void TestEvenNumbersLessThan10()
+        {
+            EvenSequence sequence = new EvenSequence(10);
+            Assert.AreEqual(new int[]{0,2,4,6,8}, sequence.ToArray());
+        }
+        
+    }
+}
